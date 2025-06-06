ddcdfde7465d967c19a8ba281ff6f052d3b52908
Rob Newsom <rwnewsom@gmail.com>
2025-06-06 16:49:58 -0400

    make move not private

--- AnimalGame.py
+++ AnimalGame.py
@@ -295,7 +295,7 @@ class AnimalGame:
                 column_distance -= 1
             return self._is_move_blocked(start_row, start_column, end_row, end_column)
 
-    def _make_move(self, start, end):
+    def make_move(self, start, end):
         """
         :param start: str- algebraic notation of starting position, e.g. 'a1' for column a row 1
         :param end: str-  algebraic notation of ending position, e.g. 'a2' for column a row 2
@@ -501,15 +501,15 @@ class TestAnimalGame(unittest.TestCase):
         """ series of tests to determine if invalid moves are flagged """
         game = AnimalGame()
         # should not be able to choose if no piece exists
-        result = game._make_move('a3', 'b2')
+        result = game.make_move('a3', 'b2')
         self.assertFalse(result)
 
         # should not be able to move opponent's piece
-        result = game._make_move('a7', 'a6')
+        result = game.make_move('a7', 'a6')
         self.assertFalse(result)
 
         # should not be able to move on top of own piece
-        result = game._make_move('a1', 'b1')
+        result = game.make_move('a1', 'b1')
         self.assertFalse(result)
 
 
@@ -517,7 +517,7 @@ class TestAnimalGame(unittest.TestCase):
     def test_validate_move_in_bounds(self):
         """should not be able to move off the board - column"""
         game = AnimalGame()
-        result = game._make_move('a1', 'h1')
+        result = game.make_move('a1', 'h1')
         self.assertFalse(result)
 
 
@@ -530,7 +530,7 @@ class TestAnimalGame(unittest.TestCase):
         """
         game = AnimalGame()
         # should be allowed to move a distance of one - orthogonal when piece movement is diagonal
-        result = game._make_move('d1', 'd2')
+        result = game.make_move('d1', 'd2')
         self.assertTrue(result)
 
         # should increment turn and change current player
@@ -543,28 +543,28 @@ class TestAnimalGame(unittest.TestCase):
 
         # should not allow a diagonal piece to move greater than 1 unit diagonally
 
-        result = game._make_move('a7', 'a5')
+        result = game.make_move('a7', 'a5')
         self.assertFalse(result)
 
         # should allow diagonal move
-        result = game._make_move('d7', 'b5')
+        result = game.make_move('d7', 'b5')
         self.assertTrue(result)
         game._print_board()
 
         # next player makes diagonal move
-        result = game._make_move('d2', 'b4')
+        result = game.make_move('d2', 'b4')
         self.assertTrue(result)
         game._print_board()
 
         # now handle capture logic!!
-        result = game._make_move('b5', 'b4')
+        result = game.make_move('b5', 'b4')
         self.assertTrue(result)
         game_state = game._get_game_state()
         self.assertEqual(game_state, 'AMETHYST_WON')
         game._print_board()
 
         # should not be able to move further this game
-        result = game._make_move('f7', 'f5')
+        result = game.make_move('f7', 'f5')
         self.assertFalse(result)
         game._print_board()
 
@@ -573,11 +573,11 @@ class TestAnimalGame(unittest.TestCase):
         a jumping piece must use all available distance
         """
         game = AnimalGame()
-        bad_jump = game._make_move('f1', 'f3')
+        bad_jump = game.make_move('f1', 'f3')
         # may not jump unless full distance
         self.assertFalse(bad_jump)
         # may jump full distance
-        good_jump = game._make_move('f1', 'f5')
+        good_jump = game.make_move('f1', 'f5')
         game._print_board()
         self.assertTrue(good_jump)
 
@@ -588,7 +588,7 @@ class TestAnimalGame(unittest.TestCase):
         game = AnimalGame()
         game._board[0][0] = None
         with self.assertRaises(UnknownValueException):
-            game._make_move('a1', 'a2')
+            game.make_move('a1', 'a2')
 
     def test_is_move_blocked(self):
         """
@@ -597,16 +597,16 @@ class TestAnimalGame(unittest.TestCase):
         game = AnimalGame()
         # set cuttlefish in front of emu
         game._print_board()
-        first_legal_move = game._make_move('a1', 'a2')
+        first_legal_move = game.make_move('a1', 'a2')
         self.assertTrue(first_legal_move)
         game._print_board()
         # make opposing move to increment turn
 
-        second_legal_move = game._make_move('a7', 'a6')
+        second_legal_move = game.make_move('a7', 'a6')
         self.assertTrue(second_legal_move)
         game._print_board()
         # try tp move emu past wombat
-        result = game._make_move('c1', 'a1')
+        result = game.make_move('c1', 'a1')
         game._print_board()
         self.assertFalse(result)
 
